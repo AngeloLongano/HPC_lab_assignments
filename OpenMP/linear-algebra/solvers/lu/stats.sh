9a65f102ca892a260b038ff64f2279a41a24b5d5
rickysixx <51907568+rickysixx@users.noreply.github.com>
2023-11-24 16:12:22 +0100

    Add verbose messages.

--- OpenMP/linear-algebra/solvers/lu/stats.sh
+++ OpenMP/linear-algebra/solvers/lu/stats.sh
@@ -3,10 +3,13 @@ echo "" > $NAME_FILE_RESULTS
 
 for file in *.c; do
     name="${file%.c}"
+    echo "Generating statistics for version ${name}..."
     echo "TEST $name" >> $NAME_FILE_RESULTS
     for size in MINI_DATASET SMALL_DATASET STANDARD_DATASET LARGE_DATASET EXTRALARGE_DATASET; do
+	echo "dataset size = ${size}..."
         echo "size $size" >> $NAME_FILE_RESULTS
         ./run.sh $name $size 4 none | tail -n 1 >> $NAME_FILE_RESULTS
     done
     printf "\n" >> $NAME_FILE_RESULTS
+    echo "----------"
 done
