deda44e31441a19022ef5eb4bcdddab220be5a8e
rickysixx <51907568+rickysixx@users.noreply.github.com>
2023-11-24 16:19:27 +0100

    Run benchmarks using polybench's `time_benchmark.sh` script.

--- OpenMP/linear-algebra/solvers/lu/stats.sh
+++ OpenMP/linear-algebra/solvers/lu/stats.sh
@@ -8,7 +8,7 @@ for file in *.c; do
     for size in MINI_DATASET SMALL_DATASET STANDARD_DATASET LARGE_DATASET EXTRALARGE_DATASET; do
 	echo "dataset size = ${size}..."
         echo "size $size" >> $NAME_FILE_RESULTS
-        ./run.sh $name $size 4 none | tail -n 1 >> $NAME_FILE_RESULTS
+        ./run.sh $name $size 4 polybench | tail -n 1 | cut -d ' ' -f 4 >> $NAME_FILE_RESULTS
     done
     printf "\n" >> $NAME_FILE_RESULTS
     echo "----------"
