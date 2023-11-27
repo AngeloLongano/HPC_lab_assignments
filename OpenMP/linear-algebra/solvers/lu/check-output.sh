bc2dfd37c9f98af2fdc9225ef7aac802c68c4604
rickysixx <51907568+rickysixx@users.noreply.github.com>
2023-11-27 10:05:45 +0100

    Fix bug in `check-output.sh`.

--- OpenMP/linear-algebra/solvers/lu/check-output.sh
+++ OpenMP/linear-algebra/solvers/lu/check-output.sh
@@ -4,7 +4,8 @@ set -e
 
 # Checks that the parallelized versions produce the same output as the sequential version
 
-DATASET_SIZES="MINI_DATASET SMALL_DATASET STANDARD_DATASET LARGE_DATASET EXTRALARGE_DATASET"
+#DATASET_SIZES="MINI_DATASET SMALL_DATASET STANDARD_DATASET LARGE_DATASET EXTRALARGE_DATASET"
+DATASET_SIZES="STANDARD_DATASET"
 UNOPTIMIZED_VERSION_NAME="lu"
 
 # use DATA_TYPE = int to avoid false positive results due to floating point arithmetics
@@ -26,7 +27,7 @@ for v in lu_opt*.c; do
 
     for n in $DATASET_SIZES; do
 	output_file_name="output.${v}.${n}.txt"
-        make --silent EXT_CFLAGS="-D${n} ${EXT_CFLAGS}" clean all run 2> "${output_file_name}"
+        make --silent EXT_CFLAGS="-D${n} ${EXT_CFLAGS}" BENCHMARK=${v%.c} clean all run 2> "${output_file_name}"
         
 	if ! diff --brief --ignore-space-change "output.${UNOPTIMIZED_VERSION_NAME}.${n}.txt" "output.${v}.${n}.txt" > /dev/null; then
             echo "ERROR! Optimized version ${v} output is different from unoptimized version for data set size = ${n}." >&2
