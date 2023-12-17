95db4a1c2f3977396769799f196d5150613e40f8
ElioLisi <62647821+ElioLisi@users.noreply.github.com>
2023-12-18 00:54:26 +0100

    fix script run.sh

--- lu_cuda/run.sh
+++ lu_cuda/run.sh
@@ -9,11 +9,11 @@ BLOCK_SIZES=32
 # Loop over dataset sizes
 for SIZE in "${DATASET_SIZES[@]}"; do
     # Loop over block sizes
-    #for BLOCK_SIZE in "${BLOCK_SIZES[@]}"; do
+    for BLOCK_SIZE in "${BLOCK_SIZES[@]}"; do
         # Execute the make command and capture the execution time
         exec_time=$(make -s EXT_CXXFLAGS="-DCUDA_TIME -DN=${SIZE} -DBLOCK_SIZE=${BLOCK_SIZE}" clean all benchmark | tail -n 1 | cut -d ' ' -f 4)
 
         # Print the result
         echo "Dataset size: ${SIZE}, Block size: ${BLOCK_SIZE}, Avg execution time: ${exec_time}s" >> results.txt
-    #done
+    done
 done
